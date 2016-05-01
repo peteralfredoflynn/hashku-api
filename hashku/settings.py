acf3404985dfa2dd6e1b34f1fca50d6bc93c4a45
Peter Flynn <fredoflynn@gmail.com>
2016-05-01 12:29:08 -0700

    add support for static files

--- hashku/settings.py
+++ hashku/settings.py
@@ -126,7 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
